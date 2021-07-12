eeb0780d706ff5e34b2627090d1faeeaea7a66d2
saurzv <saurav.raj5592+git@gmail.com>
2021-07-13 03:06:19 +0530

    Added an option to get hint.

--- script.js
+++ script.js
@@ -1,13 +1,67 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+var alert1; var alert2;
+var hint = false;
+var found = false;
+
+document.addEventListener("click", printMousePos);
+
+function printMousePos(event) {
+	var currX = event.clientX;
+	var currY = event.clientY;
+	var alert1; var alert2;
+
+	currX = (currX/w)*100;
+	currY = (currY/h)*100;
+
+	if(currY > t) {
+		alert1 = "up";
+	}
+	else{
+		alert1 = "down";
+	}
+
+	if(currX > l) {
+		alert2 = "left";
+	}
+	else {
+		alert2 = "right";
+	}
+
+	if (found){
+		document.getElementById('head').innerHTML="You've found it!";
+	}
+
+	if(hint == true && found == false){
+		document.getElementById('head').innerHTML="go " + alert1 + " and " + alert2;
+	}
+}
+
 function start(){
+	window.t = Math.floor(Math.random() * 100);
+	window.l = Math.floor(Math.random() * 100);
 	document.getElementById('start').innerHTML="Reset";
 	document.getElementById('toogle').style.display="block";
-	var top = Math.floor(Math.random() * 100);
-	var left = Math.floor(Math.random() * 100);
-	document.getElementById('container').style.top=top+"%";
-	document.getElementById('container').style.left=left+"%";
+	document.getElementById('container').style.top=t+"%";
+	document.getElementById('container').style.left=l+"%";
 }
 
 function greet() {
+
 	document.getElementById('src_img').style.display = "block";
-	document.getElementById('head').innerHTML="You've found it!"
+	document.getElementById('head').innerHTML="You've found it!";
+	found = true;
+}
+
+function toggle_hint() {
+
+	if(hint == false){
+		hint = true;
+		document.getElementById('hint').innerHTML="Hint on";
+	}
+	else{
+		hint = false;
+		document.getElementById('hint').innerHTML="Hint off";
+		document.getElementById('head').innerHTML="Find the cow";
+	}
 }
